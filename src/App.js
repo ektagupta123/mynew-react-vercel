18d9ef339a0a28cac4bc2386820ea19ea94cceb0
ektagupta123 <riyaektagupta61509@gmail.com>
2024-04-30 17:30:31 +1200

    for testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> check out more blogs.
+          Welcome to the future of thefurrymart.com <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
